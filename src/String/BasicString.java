2b2b1c4f9ad3a271ad4c3203af54c9591739229d
Navneet Kumar <jhanavneet8586@gmail.com>
2022-11-27 15:52:14 +0530

    initial commit

---
+++ src/String/BasicString.java
@@ -0,0 +1,10 @@
+package String;
+import java.util.*;
+public class BasicString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.print(s);
+        sc.close();
+    }  
+}
