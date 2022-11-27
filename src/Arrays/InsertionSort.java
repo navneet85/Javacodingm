84f2f43ccd1af3c69c1b9455bd33815afb718bb7
Navneet Kumar <jhanavneet8586@gmail.com>
2022-11-27 17:46:37 +0530

    initial commit

--- src/Arrays/InsertionSort.java
+++ src/Arrays/InsertionSort.java
@@ -3,13 +3,13 @@ package Arrays;
 public class InsertionSort {
     public static void sort(int arr[]){
         for(int i=1;i<arr.length;i++){
-            int curr=i;
+            int curr=arr[i];
             int prev=i-1;
-            while(prev>=0 && arr[curr]>arr[prev]){
+            while(prev>=0 && arr[prev]>curr){
                 arr[prev+1]=arr[prev];
                 prev--;
             }
-        arr[prev+1]=arr[curr];
+        arr[prev+1]=curr;
         }
     }
     public static void main(String[] args) {
