adccbede59a3266afc86da5c7ea971c6b1a36410
navneet85 <jhanavneet8586@gmail.com>
2023-09-05 18:20:42 +0530

    drrr

--- src/Queue/QueueUsingTwoStacks.java
+++ src/Queue/QueueUsingTwoStacks.java
@@ -11,18 +11,73 @@ public class QueueUsingTwoStacks {
             return q1.isEmpty()&& q2.isEmpty();
         }
         
-        public static void add(int n){
+        public void add(int n){
             if(!q1.isEmpty()){
                 q1.add(n);
-            }
-            if(!q2.isEmpty()){
-               q2.add(n);
-            }
+            } else {
+                q2.add(n);
+            }   
         }
 
+        public static int pop(){
+            if(isEmpty()){
+                System.out.println("Empty stack");
+                return -1;
+            }
+            int top=-1;            
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                  top=q1.remove();
+                  if(q1.isEmpty()){
+                    break;
+                  }
+                  q2.add(top);
+                }
+            }  else {
+                while(!q2.isEmpty()){
+                    top=q2.remove();
+                    if(q2.isEmpty()){
+                      break;
+                    }
+                    q1.add(top);
+                  }
+            }
+            return top;
+        }
+
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty stack");
+                return -1;
+            }
+            int top=-1;            
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                  top=q1.remove();
+                  
+                  q2.add(top);
+                }
+            }  else {
+                while(!q2.isEmpty()){
+                    top=q2.remove();
+                    
+                    q1.add(top);
+                  }
+            }
+            return top;
+        }
+      
     }
     public static void main(String[] args) {
-        
+        Stack s= new Stack();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
     }
 
 }
